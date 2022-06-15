6b283231587c2cf5edffed7a6168a4496844684c
Om Prakash Sahu <omprakashsahuit@gmail.com>
2022-06-15 20:54:19 +0530

    Add Numbers

--- TestProject/src/com/github/testproject/FirstProgram.java
+++ TestProject/src/com/github/testproject/FirstProgram.java
@@ -1,10 +1,17 @@
 package com.github.testproject;
 
+import java.util.Scanner;
+
 public class FirstProgram {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter first number: ");
+		int first = sc.nextInt();
+		System.out.print("Please enter second number: ");
+		int second = sc.nextInt();
+		System.out.println("Sum of "+first+" and "+second+" = "+(first+second));
 
 	}
 
